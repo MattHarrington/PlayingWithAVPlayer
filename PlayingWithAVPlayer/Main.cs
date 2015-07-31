34b99de4620d5595457a7c9d2ccd337fab869858
Matt Harrington <mattharrington@users.noreply.github.com>
2015-07-30 22:49:28 -0700

    Added Xamarin.Insights

--- PlayingWithAVPlayer/Main.cs
+++ PlayingWithAVPlayer/Main.cs
@@ -7,6 +7,7 @@ namespace PlayingWithAVPlayer
         // This is the main entry point of the application.
         static void Main(string[] args)
         {
+            Xamarin.Insights.Initialize("6177fa9abb40234e5363a7143c5ee7b9cf7a70a9");
             // if you want to use a different Application Delegate class from "AppDelegate"
             // you can specify it here.
             UIApplication.Main(args, null, "AppDelegate");
